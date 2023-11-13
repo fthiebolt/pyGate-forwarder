000ecb48b69b648e7b2b2b3ca69c8a1e90d36017
Francois <thiebolt@irit.fr>
2023-11-13 23:37:47 +0100

    update

---
+++ git-deploy-credentials.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# [gitlab-irit] Group access token
+# Group : xxxxx
+# expiration :
+#
+# F.Thiebolt    sep.23  initial release
+#
+
+# -- ORIGINAL SCRIPT SOURCE: https://bitbucket.org/fthiebolt/myhelpscripts/common-git-scripts
+
+_CREDSSCRIPTREV="231111"
+
+# gitlab-IRIT
+#export GIT_DEPLOY_USERNAME="<name of your deployment token>"
+#export GIT_DEPLOY_TOKEN="<token>"
+
+# github
+export GIT_DEPLOY_USERNAME="fthiebolt"
+#export GIT_DEPLOY_TOKEN="<token>"
+
